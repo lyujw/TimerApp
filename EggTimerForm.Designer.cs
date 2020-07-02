aec3f88db201beb41a689a7ae71f2a7cb60bed2b
Stan-Lyu-0309 <1962842919@qq.com>
2020-07-02 14:40:04 +0800

    first commit

---
+++ EggTimerForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace TimeApp {
+    partial class EggTimerForm {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.actionButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeTextBox.Location = new System.Drawing.Point(12, 12);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(250, 98);
+            this.timeTextBox.TabIndex = 1;
+            this.timeTextBox.Text = "03:00";
+            this.timeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.timeTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timeTextBox_KeyPress);
+            // 
+            // actionButton
+            // 
+            this.actionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actionButton.Location = new System.Drawing.Point(12, 116);
+            this.actionButton.Name = "actionButton";
+            this.actionButton.Size = new System.Drawing.Size(250, 109);
+            this.actionButton.TabIndex = 0;
+            this.actionButton.Text = "Start";
+            this.actionButton.UseVisualStyleBackColor = true;
+            this.actionButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // EggTimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 239);
+            this.Controls.Add(this.actionButton);
+            this.Controls.Add(this.timeTextBox);
+            this.Name = "EggTimerForm";
+            this.Text = "Egg Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Button actionButton;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ EggTimerForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeApp {
+    public partial class EggTimerForm : Form {
+        public EggTimerForm() {
+            InitializeComponent();
+        }
+
+        private TimeSpan remainingTime;
+
+        private void startButton_Click(object sender, EventArgs e) {
+            if (actionButton.Text == "Start") {
+                StartTimer();
+            } else if (actionButton.Text == "Reset") {
+                ReinitialiseTimer();
+            } else {
+                StopTimer();
+            }
+        }
+
+        private void StartTimer() {
+            string[] minutesAndSeconds = timeTextBox.Text.Split(':');
+            remainingTime = new TimeSpan(0,
+                int.Parse(minutesAndSeconds[0]),
+                int.Parse(minutesAndSeconds[1]));
+            timer.Start();
+            actionButton.Text = "Stop";
+        }
+
+        private void timer_Tick(object sender, EventArgs e) {
+            // Subtract one second.
+            remainingTime = remainingTime.Subtract(new TimeSpan(0, 0, 1));
+            // Any time left?
+            if (remainingTime.TotalSeconds > 0) {
+                timeTextBox.Text = remainingTime.ToString(@"mm\:ss");
+            } else {
+                TimerIsFinished();
+            }
+        }
+
+        private void TimerIsFinished() {
+            timer.Stop();
+            timeTextBox.Text = "-00:00-";
+            actionButton.Text = "Reset";
+        }
+
+        private void ReinitialiseTimer() {
+            timeTextBox.Text = "03:00";
+            actionButton.Text = "Start";
+        }
+
+        private void StopTimer() {
+            timer.Stop();
+            actionButton.Text = "Start";
+        }
+
+        private void timeTextBox_KeyPress(object sender, KeyPressEventArgs e) {
+            if (char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar)
+               || (e.KeyChar == ':' && !timeTextBox.Text.Contains(':'))) {
+                // Do nothing, letting the KeyChar be processed as normal.
+            } else {
+                e.Handled = true;
+            }
+        }
+    }
+}
